6e7b492785e4f4e4258131bc8c41323838e2d28f
JT Keller <jtkeller2@gmail.com>
2014-03-29 00:20:42 -0400

    Update Morse.java

--- Morse.java
+++ Morse.java
@@ -1,8 +1,6 @@
 /*
 Programming Assignment:  Morse Code
 Name:  James Keller
-Class Info:  1213 - 001 / Lab 6 
-Date:  3/28/2012
 Purpose:  The following Morse Code program tranlates the user's input (either a sentence or phrase) into a series dots and dashes
 provided by the "Morse.txt" file.  The data file is read and stored in array for easy accesibility and then compared to the user's
 input and translated using the Translate class.  Output is then provided to the user outlining their original input and its Morse 
